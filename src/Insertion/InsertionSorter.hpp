46b1d5f06217a6c28fe19c82f70990ad65b1d765
unknown <caleb.hansen1@wsu.edu>
2024-11-27 00:08:42 -0800

    Small fix

--- src/Insertion/InsertionSorter.hpp
+++ src/Insertion/InsertionSorter.hpp
@@ -6,7 +6,7 @@ class InsertionSorter : public Sorter
 {
 public:
 	// If right is left as default value, it will default to size - 1
-	InsertionSorter(std::vector<int>& arr = std::vector<int>(), int left = 0, int right = -1) : Sorter(arr)
+	InsertionSorter(std::vector<int>& arr, int left = 0, int right = -1) : Sorter(arr)
 	{
 		if (right == -1) right = arr.size() - 1;
 		s.push(new InsertionSortState(InsertionSortState::instr::boundsCheck, left, right, left));
